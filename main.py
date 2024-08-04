3432ada8aa2c65f6efd88c3848e307c1f889a43c
networkscientist <71866336+networkscientist@users.noreply.github.com>
2024-08-04 11:39:48 +0200

    Added first version of the scraper, though I haven't tested it yet

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import requests
+
+url = "https://grosserrat.bs.ch/component/gribs/suche"
+Itemid=105
+format=json
+filter[reiter]=GESDOK
+filter[search]=eric weber
+filter[section]
+list[ges_limit]=10
+list[dok_limit]=10
+list[ordering]=laufnr
+list[direction]=DESC
+filter[such_ges_typ]
+filter[such_ges_status]
+filter[such_von_ges]=23.07.2023
+filter[such_bis_ges]=23.07.2025
+limitstart=0
+filter[reiter]=GESDOK
+filter[search]=eric weber
+filter[section]
+list[ges_limit]=10
+list[dok_limit]=10
+list[ordering]=laufnr
+list[direction]=DESC
+filter[such_ges_typ]
+filter[such_ges_status]
+filter[such_von_ges]=23.07.2023
+filter[such_bis_ges]=23.07.2025
+limitstart=0
