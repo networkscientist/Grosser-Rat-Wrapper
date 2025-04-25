615649c4c91bdd4841b7a922cbd8ca1b12f86f9c
networkscientist <71866336+networkscientist@users.noreply.github.com>
2025-04-25 22:08:59 +0200

    tests(main): Create `test` folder and `test_main.py`

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+"""Unit Tests Module main"""
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../src')
+
+import pytest
+
+if __name__ == '__name__':
+    pytest.main()
